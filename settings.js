eeb0758356b55f84602d30a85cdec054db6a5e5f
frankmarco0911 <frankvonmarco@gmail.com>
2025-03-07 23:59:28 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'fm', // Your name
+  ownerNumber: '255757917366', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
